c29b9ac5bfcd3c41c8e70a419e01c394a51c8892
PierreSQS <nomo_messina@web.de>
2025-07-09 12:27:30 +0200

    Sec6_ASSN1-NOK: step#7a - fixed the previous issue (1ea087c4)

--- src/main/java/dev/lydtech/tracking/event/TrackingStatusUpdated.java
+++ src/main/java/dev/lydtech/tracking/event/TrackingStatusUpdated.java
@@ -1,12 +1,16 @@
 package dev.lydtech.tracking.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TrackingStatusUpdated {
     private UUID orderId;
     private Status status;
