4f8a104958f95dfb8b0c79593801f9eb137006b0
PierreSQS <nomo_messina@web.de>
2025-07-31 17:04:37 +0200

    Sec7_ASSN2-NOK: step4 - send a DispatchPreparing event to the dispatch.tracking topic

--- src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
+++ src/test/java/dev/lydtech/tracking/integration/DispatchTrackingIntegrationTest.java
@@ -3,6 +3,7 @@ package dev.lydtech.tracking.integration;
 import dev.lydtech.dispatch.event.DispatchPreparing;
 import dev.lydtech.tracking.config.TrackingConfiguration;
 import dev.lydtech.tracking.service.TrackingService;
+import dev.lydtech.tracking.util.TestEventData;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
@@ -25,9 +30,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 @EmbeddedKafka(controlledShutdown = true)
 class DispatchTrackingIntegrationTest {
 
+    private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
+
 
     @Autowired
     KafkaTestListener kafkaTestListener;
+
+    @Autowired
+    KafkaTemplate<String, Object> kafkaTemplate;
 
 
 
@@ -37,7 +47,26 @@ class DispatchTrackingIntegrationTest {
     }
 
     @Test
-    void testDispatchTrackingFlow() {
+    void testDispatchTrackingFlow() throws Exception {
+
+        // This test would verify the end-to-end flow of dispatch tracking
+        // by sending a DispatchPreparing event and checking if it is received by the Kafka listener.
+
+        log.info("Starting DispatchTrackingIntegrationTest...");
+        DispatchPreparing dispatchPreparing = TestEventData.buildDispatchPreparingEvent(UUID.randomUUID());
+
+        log.info("Sending DispatchPreparing event: {}", dispatchPreparing);
+        sendEventMessage(DISPATCH_TRACKING_TOPIC, dispatchPreparing);
+
+        log.info("Ending DispatchTrackingIntegrationTest...");
+
+    }
+
+    private void sendEventMessage(String topic, Object object) throws Exception {
+        kafkaTemplate.send(MessageBuilder
+                .withPayload(object)
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .build()).get();
     }
 
     @TestConfiguration
