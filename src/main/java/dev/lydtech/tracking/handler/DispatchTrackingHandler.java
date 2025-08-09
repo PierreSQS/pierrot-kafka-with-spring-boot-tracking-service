5a8c31931355a00d92a82835f6e0280f6868dbc5
PierreSQS <nomo_messina@web.de>
2025-08-09 16:16:20 +0200

    Sec10_Chap51: refactored the Handler to a Topic Consumer

--- src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
+++ src/main/java/dev/lydtech/tracking/handler/DispatchTrackingHandler.java
@@ -4,12 +4,19 @@ import dev.lydtech.dispatch.event.DispatchPreparing;
 import dev.lydtech.tracking.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
+@KafkaListener(
+        id = "trackingConsumerClient",
+        topics = "dispatch.tracking",
+        groupId = "tracking.dispatch.tracking.consumer",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class DispatchTrackingHandler {
 
     private final TrackingService trackingService;
@@ -21,12 +28,7 @@ public class DispatchTrackingHandler {
      *
      * @param payload The message payload received from the Kafka topic.
      */
-    @KafkaListener(
-            id = "trackingConsumerClient",
-            topics = "dispatch.tracking",
-            groupId = "tracking.dispatch.tracking.consumer",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
+    @KafkaHandler
     public void listen(DispatchPreparing payload) {
         // This method will be called when a message is received from the "dispatch.tracking" topic
         // The payload is expected to be a DispatchPreparing object
